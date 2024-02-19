db899747784ca2c771d423744c9f8cd02a36a758
md. soliman <seo.mdsoliman@gmail.com>
2024-02-19 15:32:35 +0600

    I add animation to educate my user about my website

--- src/compnents/Navbar.jsx
+++ src/compnents/Navbar.jsx
@@ -6,47 +6,68 @@ import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
 import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
 import PermContactCalendarIcon from '@mui/icons-material/PermContactCalendar';
 import "../compnents/Navbar.css";
-
+import { driver } from "driver.js";
+import "driver.js/dist/driver.css";
 
 function Navbar() {
+  const driverObj = driver({
+    showProgress: true,
+    popoverClass: 'driverjs-theme',
+    showButtons: ['next', 'previous', 'close'],
+    steps: [
+      { element: '.HomeIcon', popover: { title: 'Home', description: 'This icon is for visiting Home ' } },
+      { element: '#AboutIcon', popover: { title: 'About', description: 'This is for About section. To know about me briefly click here.' } },
+      { element: '#certificatesIcon', popover: { title: 'Certicates', description: 'To see my certificates list click this icon.' } },
+      { element: '#servicesIcon', popover: { title: 'Services', description: 'Click this icon to see what I serve for my clients' } },
+      { element: '#portfolioIcon', popover: { title: 'Portfolio', description: 'Click this icon to see what I did for client.' } },
+      { element: '#contactIcon', popover: { title: 'Contact', description: 'Click this icon to talk me.' } },
+    ]
+  });
+  driverObj.drive();
+
+
+
+
+
+
   return (
     <section  className='Navbar'>
         <div className='Navbar-Menu'>
         <a href="/">
-          <div className="icon">
-            <HomeRoundedIcon sx={{fontSize:35,textAlign:"center"}}/>
+          <div className="icon ">
+            <HomeRoundedIcon sx={{fontSize:35,textAlign:"center"}} className="HomeIcon"/>
           </div>
          </a>
 
          <a href="/#About">
-          <div className="icon">
-            <PersonIcon sx={{fontSize:35,textAlign:"center"}}/>
+          <div className="icon" >
+            <PersonIcon sx={{fontSize:35,textAlign:"center"}} id="AboutIcon"/>
           </div>
          </a>
 
 
          <a href="/certificates">
           <div className="icon">
-            <SettingsIcon  sx={{fontSize:35,textAlign:"center"}}/>
+            <SettingsIcon  sx={{fontSize:35,textAlign:"center"}} id="certificatesIcon"/>
           </div>
          </a>
 
 
          <a href="/services">
           <div className="icon">
-            <RocketLaunchIcon className="rocket-icon" sx={{fontSize:35}}/>
+            <RocketLaunchIcon className="rocket-icon" sx={{fontSize:35}} id="servicesIcon"/>
           </div>
          </a>
 
          <a href="/portfolio">
           <div className="icon">
-            <BusinessCenterIcon sx={{fontSize:35,textAlign:"center"}}/>
+            <BusinessCenterIcon sx={{fontSize:35,textAlign:"center"}} id="portfolioIcon"/>
           </div>
          </a>
 
          <a href="/#contact">
           <div className="icon">
-            <PermContactCalendarIcon  sx={{fontSize:35,textAlign:"center"}}/>
+            <PermContactCalendarIcon  sx={{fontSize:35,textAlign:"center"}} id="contactIcon"/>
           </div>
          </a>
 
